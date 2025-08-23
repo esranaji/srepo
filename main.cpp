41750a840f77daf06f768e960317bcb836cdf448
ranianagi24 <rnagi0987@gmail.com>
2025-08-23 08:56:39 +0300

    update my main

--- main.cpp
+++ main.cpp
@@ -121,9 +121,7 @@ int main() {
         cin >> n;
         vector<int> a(n), b(n) ;
 
-       for (int i = 0; i < n; i++) {
-          cin>>a[i];
-       }
+
         for (int i = 0; i < n; i++) {
             cin>>b[i];
         }
