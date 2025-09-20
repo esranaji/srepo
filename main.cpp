37f649762f4b02c9280f122663febbfb9180476f
EssraaNaji <essraanaji4@gmail.com>
2025-09-21 02:47:33 +0300

    commit 8

--- main.cpp
+++ main.cpp
@@ -15,10 +15,32 @@ int main() {
     int t ;
     cin>>t ;
     while(t--) {
-       int x,y,n ;
-        cin>>x>>y>>n;
-        int k=n;
+        int n ,k, ans=0;
+        cin>>n>>k;
+        vector< vector<int>>v (n,vector<int> (3));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                cin>>v[i][j];
+            }
+        }
+        sort(v.begin(), v.end(),[](const vector<int>& a, const vector<int>& b) {
+            return a.back() < b.back();
+        });
 
+
+        for (int i = 0; i < n; i++) {
+            if (v[i][0] <=k && v[i][1]>=k )
+                k=max(v[i][2],k);
+        }
+   cout<<k<<endl;
     }
 
+/*
+    1
+    4 6
+    1 3 2
+    2 5 5
+    6 10 10
+    8 12 12
+ */
 }
