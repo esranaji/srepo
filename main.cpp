57c5040fd2872eae82d79f60bc0982b0f89aa1d4
EssraaNaji <essraanaji4@gmail.com>
2025-09-21 16:11:14 +0300

    update from rebase

--- main.cpp
+++ main.cpp
@@ -1,48 +1,38 @@
-#include <bits/stdc++.h>
-#define  ll long long
-using namespace std;
-
-
-
-const int MOD = 1e9 + 7;
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t ;
-    cin>>t ;
-    while(t--) {
-        int n ,k, ans=0;
-        cin>>n>>k;
-        vector< vector<int>>v (n,vector<int> (3));
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                cin>>v[i][j];
+            cin >> a[i];
+        }
+        vector<int> P(n+1, 0);
+        for (int i = 1; i <= n; i++) {
+            P[i] = (P[i-1] + a[i-1]) % 3;
+        }
+        bool found = false;
+        for (int l = 1; l <= n-2; l++) {
+            for (int r = l+1; r <= n-1; r++) {
+                int s1 = P[l] % 3;
+                int s2 = (P[r] - P[l] + 3) % 3;
+                int s3 = (P[n] - P[r] + 3) % 3;
+                if ((s1 == s2 && s2 == s3) || (s1 != s2 && s1 != s3 && s2 != s3)) {
+                    cout << l << " " << r << endl;
+                    found = true;
+                    break;
+                }
             }
+            if (found) break;
         }
-        sort(v.begin(), v.end(),[](const vector<int>& a, const vector<int>& b) {
-            return a.back() < b.back();
-        });
-
-/*
- * pdate from rebase
- */
-        for (int i = 0; i < n; i++) {
-            if (v[i][0] <=k && v[i][1]>=k )
-                k=max(v[i][2],k);
+        if (!found) {
+            cout << "0 0" << endl;
         }
-   cout<<k<<endl;
     }
-
-/*
-    1
-    4 6
-    1 3 2
-    2 5 5
-    6 10 10
-    8 12 12
- */
+    return 0;
 }
